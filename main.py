18055a799b0b066989d22f54d0b1b35174b18c44
zakupra <zakupra@gmail.com>
2024-10-12 19:59:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# 1st program
+print(9 ** 0.5 * 5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3nd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+# 4nd program
+print(int(float('123.456') * 10) % 10)
